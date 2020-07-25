4b500768092be42cf128bd3d1feacf92155fae3a
MT Ekleel <63772286+MTE1991@users.noreply.github.com>
2020-07-25 18:24:46 +0600

    Update plt7.py

--- plt7.py
+++ plt7.py
@@ -8,13 +8,16 @@ tests = [10923,10625,13362,12898,12050,12398,12027,10446]
 deaths = [34,37,50,41,42,50,35,38]
 fig, ax = plt.subplots(2)
 
+fig.set_figheight(20)
+
 ax[0].bar(dates, tests, width=0.5, label="No. of tests")
 ax[0].bar(dates, pos_cases, width=0.5, label="Positive cases")
 
 ax[0].set_title('COVID 19 scenario in BD (18-25 July, 2020)')
 ax[0].legend()
 
-ax[1].bar(dates, deaths, width=0.5, label="Deaths")
+ax[1].set_title("Reported Deaths")
+ax[1].bar(dates, deaths, alpha=0.75, width=0.5, label="Deaths", color='c', edgecolor='r')
 ax[1].set_ylabel("No. of deaths")
 
 plt.show()
