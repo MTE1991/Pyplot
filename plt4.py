e02366c78ca243bf650e324e2be3e689944654c3
MT Ekleel <63772286+MTE1991@users.noreply.github.com>
2020-08-12 14:15:09 +0000

    Update plt4.py

--- plt4.py
+++ plt4.py
@@ -2,13 +2,11 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 c = 3e8
-
-v = np.linspace(0,3e8,100) 
-
-k = 1/np.sqrt(1-v**2/c**2) # Lorentz factor
+v = np.linspace(0,2.999e8,100) 
+gamma = 1/np.sqrt(1-v**2/c**2) # Lorentz factor
 
 plt.title("Lorentz factor - Velocity Graph")
-plt.plot(v,k)
+plt.plot(v,gamma)
 plt.xlabel("Velocity (m/s)")
-plt.ylabel("Lorentz factor")
+plt.ylabel("Lorentz factor $\gamma$")
 plt.show()
