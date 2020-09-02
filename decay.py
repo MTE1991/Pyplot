8f71a0fb7f36b11fe530285f45d3d8ddf2ad196b
MT Ekleel <63772286+MTE1991@users.noreply.github.com>
2020-09-02 17:17:17 +0600

    Update decay.py

--- decay.py
+++ decay.py
@@ -11,6 +11,7 @@ decay_u235 = 9.72e-10
 t = np.linspace(0,3e10,500) # years
 
 # Remnants after a certain time (t):
+
 n2 = n_0 * np.exp(-decay_u238 * t)
 n3 = n_0 * np.exp(-decay_u235 * t)
 
@@ -21,6 +22,6 @@ ax.plot(t, n3, label="U-235")
 ax.legend()
 
 plt.title("Radioactive Decay of Uranium")
-plt.xlabel("time (years)")
+plt.xlabel("time ($10^{10}$ years)")
 plt.ylabel("No. of atoms present (x$10^{23}$)")
 plt.show()
